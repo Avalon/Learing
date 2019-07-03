4829e37904ba2a9cb7481855e7af2eb05b1701b5
St0rma1 <avalonsama@163.com>
2019-07-03 12:24:03 +0800

    test

---
+++ Shell/printf.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+printf "%-5s %-10s %-4s\n" No. Name Work
+printf "%-5s %-10s %-4.2f\n" 1 Sarath 80.3456
+printf "%-5s %-10s %-4.2f\n" 2 James 72.24
+printf "%-5s %-10s %-4.2f\n" 3 Me 50
+
